01923542e7385729530f591c915685c61f1932f8
Krishanenterprises <krishanenterprisesofficial@gmail.com>
2026-08-18 16:47:36 +0530

    Add Category 09 gifting box products

---
+++ category9.js
@@ -0,0 +1,7 @@
+const category9Products = [
+  {id:"KE-GB-001",category:"gifting-boxes",name:"Bamboo-less Gift Box",images:["assets/images/gifting-boxes/KE-GB-001.jpg"],specs:{Contents:"Metal Ramlalla Idol & 65 Mixed Incense Products",Fragrance:"Sandalwood"}},
+  {id:"KE-GB-002",category:"gifting-boxes",name:"Mix Box",images:["assets/images/gifting-boxes/KE-GB-002.jpg"],specs:{Contents:"Rose / Dhuna, Sandalwood, Jasmine / Guggul, Oudh / Loban"}},
+  {id:"KE-GB-003",category:"gifting-boxes",name:"Small Mix Box with Idol",images:["assets/images/gifting-boxes/KE-GB-003.jpg"],specs:{}},
+  {id:"KE-GB-004",category:"gifting-boxes",name:"Small Mix Box",images:["assets/images/gifting-boxes/KE-GB-004.jpg"],specs:{}}
+];
+products.push(...category9Products);
